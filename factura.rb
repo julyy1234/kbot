5adae0ebc404d9b5eeb8dadcd9670bf11850eb58
Julian Sanchez <juliansanchez@summant.com>
2020-08-26 11:22:33 +0000

    Creación hashmap de descuentos

--- factura.rb
+++ factura.rb
@@ -1,6 +1,7 @@
 class Factura
     def initialize(cant,precio,estado)
         @impuesto= {"UT"=> 0,0685, "NV"=> 0,08, "TX"=> 0,0625, "AL"=> 0,04, "CA"=> 0,0825}
-        
+        @descuento= {1000=> 0,03, 5000=> 0,05, 7000=> 0,07, 10000=> 0,1, 50000=> 0,15}
+
     end
 end
